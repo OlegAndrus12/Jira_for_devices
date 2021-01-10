a5289b928dbabfb77d13f9151f1ff0c9ed05658f
OlegAndrus12 <andrusoleg123@gmail.com>
2021-01-10 20:41:41 +0200

    Models and first migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.TextField('Username')
+    role = models.TextField('Role')
+
+    def __str__(self):
+        return "Username : {}, role : {}".format(self.username, self.role)
+    
+    class Meta:
+        verbose_name =  "User"
+        verbose_name_plural = "Users"
+    
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import User
 import random
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'main/about.html')
+    users = User.objects.all()
+    return render(request, 'main/about.html', {'users' : users} )
